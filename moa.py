465202251434cf803757ea93f667a8c500bdf814
LarsBergh <bergh.lars@hotmail.com>
2020-10-08 22:12:59 +0200

    kfold log loss parm search

--- moa.py
+++ moa.py
@@ -8,7 +8,7 @@ import numpy as np
 import tensorflow as tf
 import seaborn as sns
 from tensorflow.keras.losses import BinaryCrossentropy
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.decomposition import PCA
 from sklearn.metrics import log_loss
 from tensorflow.keras.layers import Dense, Dropout, LSTM, Activation, ActivityRegularization
@@ -93,36 +93,33 @@ def pca(df, df_sub, var_req, pca_type):
     #Return PCA df
     X_pca = pd.DataFrame(PCA(n_components=pc, random_state=0).fit_transform(pca),columns=cols)
     X_sub_pca = pd.DataFrame(PCA(n_components=pc, random_state=0).fit_transform(pca_sub),columns=cols)
-    return X_pca, X_sub_pca, pc
+    return X_pca, X_sub_pca
 
 #Apply PCA on gene/cell columns of X and X_submit
-g_df, g_df_sub, g_comp = pca(df=X, df_sub=X_submit, var_req=0.99, pca_type="gene")
-c_df, c_df_sub, c_comp = pca(df=X, df_sub=X_submit, var_req=0.99, pca_type="cell")
+g_var_req, c_var_req = 0.7, 0.9
 
-print("Gene df", g_df.shape, g_df_sub, " amount of components with 80% var explained: " , g_comp)
-print("Cell df", c_df.shape,  c_df_sub.shape, " amount of components with 90% var explained: " , c_comp)
+g_df, g_df_sub = pca(df=X, df_sub=X_submit, var_req=g_var_req, pca_type="gene")
+c_df, c_df_sub = pca(df=X, df_sub=X_submit, var_req=c_var_req, pca_type="cell")
+
+print("Gene df", g_df.shape, "Gene df submit: ", g_df_sub.shape, " with", g_var_req*100, "% var explained: ")
+print("Cell df", c_df.shape, "Cell df",  c_df_sub.shape, " with", c_var_req*100, "% var explained: ")
 
 def encode_scale_df(df, cols):
-    print("df before ecode/scale ", df)
-
-    #Encode variables
-    enc = pd.get_dummies(df, columns=cols)
-
-    #Drop encoded variable columns for replacement
+    #Create encode df and drop encoded vars from df
+    enc = pd.get_dummies(df[cols], columns=cols)
+    
+    #Drop encoded vars from df
     df = df.drop(cols, axis=1)
 
     #Scale all variables that are left (all numerical)
     df=(df-df.min())/(df.max()-df.min())
-    print("Min/Max per column of X --> Confirming that cell and gene columns are normalized")
-    print(pd.concat([df.min(),df.max()],axis=1))
 
     #Compile new df from encoded vars & scaled vars
     df = pd.concat([enc, df],axis=1)
-    print("X shape after encoding and dropping: " , df.shape)
-    print("X column names after encoding ", df.columns)
-    print("df after ecode/scale", df)
+
     return df
 
+#Combine main columns with PCA columns into 1 dataframe for X and X_submit
 main_cols = ["cp_time", "cp_dose", "cp_type"]
 X = pd.concat([X[main_cols], g_df, c_df],axis=1)
 X_submit = pd.concat([X_submit[main_cols], g_df_sub, c_df_sub],axis=1)
@@ -137,13 +134,13 @@ y = pd.concat([y, y_binary], axis=1)
 
 #------------------------ Splitting data ------------------------#
 #Train and validation data split
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+"""X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.25, random_state=0)
 
 #Print resulting shapes of splitted datasets
 print("X_train, y_train shape: ", X_train.shape, y_train.shape)
 print("X_val, y_val shape: ", X_val.shape, y_val.shape)
-print("X_test, y_test shape: ", X_test.shape, y_test.shape)
+print("X_test, y_test shape: ", X_test.shape, y_test.shape)"""
 
 #------------------------ Ensure model reproducibility ------------------------
 #Start tensorflow session and set np and tensorflow seeds for this session
@@ -151,6 +148,50 @@ np.random.seed(0)
 tf.random.set_seed(0)
 sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph())
 tf.compat.v1.keras.backend.set_session(sess)
+
+#------------------------ Parameter selection ------------------------#
+def select_random_parameters(n_param_sets):
+    """fils n_param_sets of dictionaries with parameters that can be used for model building"""
+
+    """layers = np.array([x for x in range(1, 5)]) #Layers 1 to 5
+    activations = ["relu", "sigmoid", "softmax", "softplus", "softsign", "tanh", "selu", "elu", "linear"] #All acti except for "exponential" because gives NA loss
+    dropout = [x for x in np.round(np.arange(0.1, 1, 0.1),1)] #dropout 0.1 to 0.9
+    neurons = [8, 16, 32]
+    epochs = [1,2,3,4]
+    optimizers = ["adadelta", "adagrad", "adam", "adamax", "ftrl", "nadam", "rmsprop", "sgd", SGD(lr=0.05, momentum=0.98)]
+"""
+    #"selu", softmax, 3, layers, 64 neuron, 25 epoch, adam
+
+    #Define lists with parameter options
+    layers = [3] #Layers 1 to 5
+    acti_hid = ["selu"] #All acti except for "exponential" because gives NA loss
+    acti_out = ["softmax"] #All acti except for "exponential" because gives NA loss
+    dropout = [0.2] #dropout 0.1 to 0.9
+    neurons = [64]
+    epochs = [25]
+    optimizers = ["adam"]
+
+    #Create dictionary of parameters
+    para_dic = {"lay": layers, "acti_hid": acti_hid, 
+                "acti_out": acti_out, "neur": neurons, 
+                "drop": dropout, "epo": epochs,
+                "opti": optimizers}
+
+    #Create list for random parameter sets
+    params_set_list = []
+
+    for i in range(n_param_sets):
+            
+            params = {}
+
+            #Randomly select parameters from parameter lists
+            for key, parameters in para_dic.items():
+                params[key] = random.choice(parameters)
+            
+            #Append dictionary of random parameters to list
+            params_set_list.append(params)
+    
+    return params_set_list
 
 #------------------------ Model creation ------------------------#
 def create_model(X, y, lay, acti_hid, acti_out, neur, drop, epo, opti):
@@ -162,6 +203,7 @@ def create_model(X, y, lay, acti_hid, acti_out, neur, drop, epo, opti):
     print("Neuron count per layer: ", neur)
     print("Dropout value: ", drop)
     print("Epoch count: ", epo)
+    print("Optimizer: ", opti)
 
     #Create model
     model = Sequential()
@@ -178,86 +220,76 @@ def create_model(X, y, lay, acti_hid, acti_out, neur, drop, epo, opti):
     model.compile(optimizer=opti, loss='binary_crossentropy', metrics=["acc"]) 
 
     #Fit and return model
-    model.fit(X, y, batch_size=8, epochs=epo)
+    model.fit(X, y, batch_size=16, epochs=epo)
     return model
 
 
-#------------------------ Random search ------------------------#
-def random_search(model_count):
+
+#------------------------ K fold for given parameter lists ------------------------#
+def k_fold(X, y, n_fold, param_set_list):
     best_loss = 100000
     best_params = {}
 
-    """layers = np.array([x for x in range(1, 5)]) #Layers 1 to 5
-    activations = ["relu", "sigmoid", "softmax", "softplus", "softsign", "tanh", "selu", "elu", "linear"] #All acti except for "exponential" because gives NA loss
-    dropout = [x for x in np.round(np.arange(0.1, 1, 0.1),1)] #dropout 0.1 to 0.9
-    neurons = [8, 16, 32]
-    epochs = [1,2,3,4]"""
+    #Loop over parameter dic list 
+    for i in range(len(param_set_list)):  
+        
+        val_loss_total = 0
 
-    layers = [2,3,4] #Layers 1 to 5
-    acti_hid = ["elu"] #All acti except for "exponential" because gives NA loss
-    acti_out = ["softmax"] #All acti except for "exponential" because gives NA loss
-    dropout = [0.2] #dropout 0.1 to 0.9
-    neurons = [16, 32, 64]
-    epochs = [10, 20, 30]
-    optimizers = ["adadelta", "adagrad", "adam", "adamax", "ftrl", "nadam", "rmsprop", "sgd", SGD(lr=0.05, momentum=0.98)]
+        #Get current parameter dic  
+        par_dic = param_set_list[i]
 
-    for model in range(0, model_count):
-        #Randomly select parameters from parameter lists
-        params = {}
+        #For each parameter sec Kfold
+        for train_i, test_i in KFold(n_splits=n_fold, shuffle=True, random_state=0).split(X):
+                
+            #Define train and test for Kfolds
+            X_train = X.iloc[train_i,:]
+            X_test = X.iloc[test_i,:]
+            y_train = y.iloc[train_i,:]
+            y_test = y.iloc[test_i,:]
 
-        para_dic = {"lay": layers, "acti_hid": acti_hid, 
-                    "acti_out": acti_out, "neur": neurons, 
-                    "drop": dropout, "epo": epochs,
-                    "opti": optimzers}
+            #Create a model for each split 
+            model = create_model(X=X_train, y=y_train, 
+                    lay=par_dic["lay"], 
+                    acti_hid=par_dic["acti_hid"], 
+                    acti_out=par_dic["acti_out"], 
+                    neur=par_dic["neur"], 
+                    drop=par_dic["drop"], 
+                    epo=par_dic["epo"],
+                    opti=par_dic["opti"])
 
-        for key, parameters in para_dic.items():
-            params[key] = random.choice(parameters)
+            #Predict y_val for each fold for each parameter set
+            y_test_pred = model.predict(X_test)
+            
+            """"Rewrite as 1 line"""
+            #Calculate and print log loss for validation and test 
+            val_loss = 0
+            for col in range(len(y_test.columns)):
+                val_loss += log_loss(np.array(y_test.iloc[:,col]), y_test_pred[:,col], labels=[0,1]) / y_test.shape[1]
+            print("Val loss: ",val_loss)
+            val_loss_total += val_loss
 
-        #Create model and fit on data on randomly selected parameters
-        model = create_model(X=X_train, y=y_train, 
-                            lay=params["lay"], 
-                            acti_hid=params["acti_hid"], 
-                            acti_out=params["acti_out"], 
-                            neur=params["neur"], 
-                            drop=params["drop"], 
-                            epo=params["epo"],
-                            opti=params["optimizers"])
-
-        #Get validation loss/acc
-        val_loss = model.evaluate(X_val, y_val, batch_size=8)
-        test_loss = model.evaluate(X_test, y_test, batch_size=8)
-
-        if test_loss[0] < best_loss:
-            print("Better loss found. From ", best_loss ," to ", test_loss[0])
-            best_loss = test_loss[0]
-            best_params = params
+        #Find best loss of averaged Kfolds for all parameter sets
+        if val_loss_total/n_fold < best_loss:
+            print("Better loss found. From ", best_loss ," to ", val_loss_total/n_fold)
+            best_loss = val_loss_total/n_fold
+            best_params = par_dic
             best_model = model
+            
+    return best_params, best_loss, best_model
 
-    return (best_params, best_loss, model)
+#Select random parameters
+param_set_list = select_random_parameters(1)
 
-#Run random search 
-best_params, best_loss, model = random_search(model_count=1)
+#Apply K_fold on the randomly selected parameter sets
+best_params, best_loss, model = k_fold(X=X, y=y, n_fold=3, param_set_list=param_set_list)
+
+#Print best parameters and best loss
 print("Best params: ", best_params, " Best loss: ", best_loss)
 
-#Calculate Binary Crossentropy of model without last column
-y_val_pred = model.predict(X_val)
-y_test_pred = model.predict(X_test)
-#%%
-#------------------------ Calculate log loss -----------------------------#
-val_loss = 0
-test_loss = 0
-for col in range(len(y_val.columns)):
-    val_loss += log_loss(np.array(y_val.iloc[:,col]), y_val_pred[:,col], labels=[0,1]) / y_val.shape[1]
-    test_loss += log_loss(np.array(y_test.iloc[:,col]), y_test_pred[:,col], labels=[0,1]) / y_test.shape[1]
-
-print("Val loss: ",val_loss, "Test loss: ", test_loss)
-#%%
 #Predict values for submit
 y_submit = model.predict(X_submit)
 
 #Create dataframe and CSV for submission
 submit_df = np.concatenate((np.array(X_id_submit).reshape(-1,1), y_submit[:,:206]), axis=1)
 pd.DataFrame(submit_df).to_csv(path_or_buf=output_folder + "submission.csv", index=False, header=y_cols)
-
-
 # %%
