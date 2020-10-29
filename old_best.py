2ab69dd92828ec9a2404946b23ddb6a0d1450040
LarsBergh <bergh.lars@hotmail.com>
2020-10-29 17:24:32 +0100

    visuals

--- old_best.py
+++ old_best.py
@@ -56,11 +56,124 @@ X_submit.drop("sig_id", axis=1, inplace=True)
 print("X, y, X_submit shape after id remove: " ,X.shape, y.shape, X_submit.shape)
 
 #------------------------ Exporatory Data Analysis ------------------------#
+#%%
 #Show distribution of amount of labels per row
-#sns.displot(y.sum(axis=1))
 print(y.sum(axis=1).value_counts().sort_index(axis=0))
 print(100-((303+55+13+6)/len(y)*100), " percent has 0,1 or 2 labels")
 
+#Show distribution of amount of labels per row
+if is_kaggle == False:
+    #rcParams.update({'figure.autolayout': True})
+    target_counts = pd.concat([pd.Series([x for x in range(8)]), y.sum(axis=1).value_counts().sort_index(axis=0)], axis=1, keys=["targets per drug", "amount of drugs"]).fillna(0)
+    plot = sns.barplot(data= target_counts, x="targets per drug", y="amount of drugs")
+    plot.set_title('Amount of targets per drug admission')
+    for index, row in target_counts.iterrows():
+        plot.text(row.name,row["amount of drugs"] + 40, int(row["amount of drugs"]), color='black', ha="center")
+    plt.tight_layout()
+    plot.figure.savefig("figs/target_count_plot.jpg")
+
+#%%
+def plot_gene_cell_dist(df, scaler_type=None):
+    cols = ["g-0","g-175","g-363","g-599", 
+            "c-4", "c-33", "c-65", "c-84"]
+    # Create four polar axes and access them through the returned array
+    fig, axs = plt.subplots(2,4, figsize=(20,10))
+    count = 0
+    if scaler_type != None:
+        fig.suptitle("Columns scaled with " + SC_TYPE, fontsize=22)
+    else:
+        fig.suptitle("Gene expression and cell viability distributions", fontsize=22)
+
+    for i in range(0, 2):
+        for j in range(0, 4):
+            if count >= 4:
+                axs[i, j].hist(x=X.loc[:, cols[count]], bins=50, color="#E1812B")
+            else:
+                axs[i, j].hist(x=X.loc[:, cols[count]], bins=50, color="#3174A1")
+
+            axs[i, j].set_title("Distribution " + cols[count])
+            count += 1
+    plt.tight_layout()
+    fig.savefig("figs/genes_cells-dist.jpg")
+
+plot_gene_cell_dist(df=X)
+
+#%%
+def plot_skew_kurtosis(df):
+    #Calculate skewness and kurtosis
+    kurtosis = X.loc[:,"g-0":].kurtosis(axis=0)
+    skew = X.loc[:,"g-0":].skew(axis=0)
+
+    #Split kurtosis and skew values into bins
+    bin_skew = [-np.inf, -1, -0.5, 0.5, 1, np.inf]
+    lab_skew = ['high skew left', 'low skew left', 'approx symmetric', 'low skew right', 'high skew right']
+    
+    bin_kurt = [-np.inf, -2, 2, np.inf]
+    lab_kurt = ['platykurtic', 'mesokurtic', 'leptokurtic']
+
+    #Create skew label and kurtosis label columns
+    skew_labeled = pd.cut(skew, bins=bin_skew, labels=lab_skew, ordered=False)
+    kurtosis_labeled = pd.cut(kurtosis, bins=bin_kurt, labels=lab_kurt, ordered=False)
+    
+    #Create full skew, kurtosis dataframe
+    skew_kurt_df = pd.concat([skew, skew_labeled, kurtosis, kurtosis_labeled], 
+                        keys=["Skew", "Skew labeled", "Kurtosis", "Kurtosis labelled"], axis=1)
+
+    #Split into cell and gene skew/kurtosis df
+    gene = skew_kurt_df.loc["g-0":"g-771", :]
+    cell = skew_kurt_df.loc["c-0":"c-99",:]
+
+    print(gene, cell)
+
+    #Create skewness visual
+    fig, axs = plt.subplots(1,2, figsize=(10,5))
+    fig.suptitle("Skewness and kurtosis of gene and cell values", fontsize=22)
+    
+    axs[0].hist(x=gene["Skew"], bins=50, color="#E1812B")
+    axs[0].set_title("Gene skewness values")
+
+    #print(gene["Skew labeled"].value_counts().values)
+    gene_bar = gene["Skew labeled"].value_counts().reset_index()
+    bar_skew = sns.barplot(data=gene_bar, x="index", y="Skew labeled", ax=axs[1], color="#E1812B")
+    bar_skew.set_xticklabels(bar_skew.get_xticklabels(),rotation=15,ha="right",rotation_mode='anchor')
+      
+    #axs[0, 1].(x= , , height=10000)
+    #axs[0, 1].set_title("Gene skewness categories")
+
+    #axs[i, j].hist(x=gene["Skew"], bins=50, color="#E1812B")
+    
+    #axs[i, j].hist(x=X.loc[:, cols[count]], bins=50, color="#3174A1")
+
+            #axs[i, j].set_title("Distribution " + cols[count])
+
+    #plt.tight_layout()
+    #fig.savefig("figs/genes_cells-dist.jpg")
+
+plot_skew_kurtosis(df=X)
+
+#%%
+
+
+bins = []
+labels = ['', '2-18', '18-35', '35-65', '65+']
+
+df['AgeRange'] = pd.cut(df['Age'], bins=bins, labels=names)
+
+
+
+
+
+
+
+print(kurtosis_gene.mean(), kurtosis_gene.median())
+
+
+kurtosis_cell = X.loc[:,"c-0":"c-99"].kurtosis(axis=1)
+skew_cell = X.loc[:,"c-0":"c-99"].skew(axis=1)
+
+
+
+#%%
 #------------------------ Parameters ------------------------#
 #Encoding type --> "map", "dummy"
 ENC_TYPE = "map"
@@ -118,19 +231,7 @@ X = encode_df(df=X, encoder_type=ENC_TYPE)
 X_submit = encode_df(df=X_submit, encoder_type=ENC_TYPE)
 
 print("X after encoding it with", ENC_TYPE, X)
-#%%
-def plot_scaled_df(df, scaler_type):
-    # Create four polar axes and access them through the returned array
-    fig, axs = plt.subplots(3,3, figsize=(20,20))
-    count = 0
-    fig.suptitle("Columns scaled with " + SC_TYPE, fontsize=22)
-    for i in range(0, 3):
-        for j in range(0, 3):
-            axs[i, j].hist(x=X.loc[:, X.columns[count+ list(X.columns).index("g-0")]], bins=50)
-            axs[i, j].set_title("Distribution G-" + str(count))
-            count += 1
 
-plot_scaled_df(df=X, scaler_type=SC_TYPE)
 #%%
 #------------------------ Splitting data ------------------------#
 #Train and validation data split
